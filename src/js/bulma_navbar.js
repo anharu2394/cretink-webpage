f80ac793a05fc9675e2d5ab02fec62f2f628bd65
安藤春香 <haruan2394@gmail.com>
2018-05-11 23:40:24 +0900

    windowに。

--- src/js/bulma_navbar.js
+++ src/js/bulma_navbar.js
@@ -1,4 +1,4 @@
- addEventListener("load",function(){
+window.addEventListener("load",function(){
   // Get all "navbar-burger" elements
   var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
