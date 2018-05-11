2221e06c247eea040e14ed06ae787a0ac3b8e974
安藤春香 <haruan2394@gmail.com>
2018-05-11 23:30:15 +0900

    menu表示できた

--- src/js/bulma_navbar.js
+++ src/js/bulma_navbar.js
@@ -1,9 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-
+ addEventListener("load",function(){
   // Get all "navbar-burger" elements
   var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-  // Check if there are any navbar burgers
+  // Check if there are any nav burgers
   if ($navbarBurgers.length > 0) {
 
     // Add a click event on each of them
--- src/js/header.js
+++ src/js/header.js
@@ -3,21 +3,26 @@ import { Link, Route, Switch, Redirect, location } from "@hyperapp/router"
 
 export const Header = () => (actions,state) =>  (
     <header>
-        <nav class="navbar is-transparent is-light " role="navigation" aria-label="main navigation">
+        <nav class="navbar is-transparent is-light" role="navigation" aria-label="main navigation">
         <div class="container">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="/">
-        <h1 class="title">Cretink</h1>
-    </a>
-  </div>
-  <div class="navbar-menu is-active" id="navMenu">
-    <div class="navbar-start">
-        <Link class="navbar-item"to="/">Home</Link>
-        <Link class="navbar-item" to="/about">About</Link>
-        <Link class="navbar-item" to="/demo">Demo</Link>
+          <div class="navbar-brand">
+            <a class="navbar-item" href="/">
+                <h1 class="title">Cretink</h1>
+            </a>
+     <div class="navbar-burger burger" data-target="navMenubd-example">
+      <span></span>
+      <span></span>
+      <span></span>
     </div>
-  </div>
-    </div>
-</nav>
+          </div>
+     <div id="navMenubd-example" class="navbar-menu">
+            <div class="navbar-start">
+                <Link class="navbar-item"to="/">Home</Link>
+                <Link class="navbar-item" to="/about">About</Link>
+                <Link class="navbar-item" to="/demo">Demo</Link>
+            </div>
+          </div>
+            </div>
+        </nav>
     </header>
 )
