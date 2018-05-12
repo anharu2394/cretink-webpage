331056b437c0ae8e1dcd4acbbef8356b6569cf26
安藤春香 <haruan2394@gmail.com>
2018-05-12 17:54:34 +0900

    homeページ完成イメージボタン

--- src/js/content.js
+++ src/js/content.js
@@ -4,15 +4,24 @@ import {Demo} from "./pages/demo"
 
 export const Home = () => (
     <div>
-        <section class="hero is-medium is-primary is-bold">
+        <section class="hero is-large is-primary ">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+            <h1 class="title">
+    Cretink of WebServiceはWebサービスのアイデアを提案してくれるAIです。
+            </h1>
+        </div>
+        </div>
+        </section>
+        <section class="hero is-medium is-info">
         <div class="hero-body">
             <div class="container">
-            <h1 class="title">
-                Cretink
-            </h1>
-            <h2 class="subtitle">
-                Home
+            <h2 class="subtitle has-text-centered">
+                Cretink of WebServiceの完成イメージを作成しました。
             </h2>
+            <div class="buttons has-addons is-centered">
+                <Link class="button is-info is-large is-rounded is-inverted" to="/demo">完成イメージへ！</Link>
+            </div>
         </div>
         </div>
         </section>
