fe5c31dd30e681aeabc4cc7b9bf4823021b70779
安藤春香 <haruan2394@gmail.com>
2018-05-12 18:16:14 +0900

    aタグをlinkタグへ

--- src/js/header.js
+++ src/js/header.js
@@ -14,9 +14,9 @@ export const Header = () => (actions,state) =>  (
         <nav class="navbar is-transparent is-light" role="navigation" aria-label="main navigation">
         <div class="container">
           <div class="navbar-brand">
-            <a class="navbar-item" href="/">
+            <Link class="navbar-item" to="/">
                 <h1 class="title">Cretink</h1>
-            </a>
+            </Link>
             <div class="navbar-toggle navbar-burger" id="navbar-toggle">    
                 <span></span>
                 <span></span>
